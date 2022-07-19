29c5643580f0ba30426f290dd19f62fdac8f4aa6
LuBu-0 <luca_busacca@hotmail.com>
2022-07-19 10:20:22 +0200

    update project name

--- SmartCitySim/src/main/java/it/synclab/smartparking/SmartCitySim.java
+++ SmartCitySim/src/main/java/it/synclab/smartparking/SmartCitySim.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartparkingApplication {
+public class SmartCitySim {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartparkingApplication.class, args);
+		SpringApplication.run(SmartCitySim.class, args);
 		
 	}
 }
