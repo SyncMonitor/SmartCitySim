79a6e9ef39e677a769d3bc0849d0dcb6db595ddc
Leonardo Costa <76658858+lerro-lerro@users.noreply.github.com>
2022-11-30 18:12:06 +0100

    Update PostgreClient.java
    update parameter for connect to db

--- SmartCitySim/src/main/java/it/synclab/smartparking/config/datasource/PostgreClient.java
+++ SmartCitySim/src/main/java/it/synclab/smartparking/config/datasource/PostgreClient.java
@@ -11,21 +11,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class PostgreClient {
 
-	Properties props = new Properties();
-	@Value("${postgres.jdbc.url}")
-	private String jdbcURL;
-
-	@Value("${postgres.username}")
-	private String username;
-
-	@Value("${postgres.password}")
-	private String password;
-
 	private Connection conn = null;
 
 	private static final Logger logger = LogManager.getLogger(PostgreClient.class);
 
-	public PostgreClient() {
+	public PostgreClient(String jdbcURL, String username, String password) {
 		logger.info("PostgreClient START - trying connection to PosgresDB");
 		try {
 			conn = DriverManager.getConnection(jdbcURL,username,password);
